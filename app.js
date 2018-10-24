a73ec457297674a5906f49da74811e96589d1b71
bugall <769088641@qq.com>
2018-10-24 11:49:22 +0800

    fix

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 'use strict';
 const config = require('./config');
+const api = require('./lib/api')
 
 const dbConn = require('./database/conn');
 
 // pre-start, check service state
 dbConn.authenticate().then(() => {
-    console.log('Reward Service Start');
+    console.log('Wallet Service Start');
     // start
-    return reward.start();
+    // return reward.start();
 }).catch((err) => {
     console.log(err, 'reward');
 });
+
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', api.payCallback);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(config.server.port);
---
+++ database/models/pay.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+
+module.exports = (conn) => conn.define('tb_pay', ({
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    order_id: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    address: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    total: {
+        type: Sequelize.NUMBER,
+        allowNull: false
+    },
+    channel: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    // 0 待创建钱包, 5 待转账, 10 转账完成
+    status: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        menu: [0, 5, 10, 15],
+        default: 0
+    },
+    create_wallet_time: {
+        type: Sequelize.DATE,
+        allowNull: true
+    },
+    exchange_export_time: {
+        type: Sequelize.DATE,
+        allowNull: true
+    },
+    created_at: Sequelize.DATE,
+    updated_at: Sequelize.DATE
+}), {
+    tableName: 'tb_pay'
+});
