0c433dd9effcb4f5a2db409f5ce6ce431707ef82
bugall <769088641@qq.com>
2018-10-24 15:19:48 +0800

    add color output

--- app.js
+++ app.js
@@ -4,16 +4,8 @@ const api = require('./lib/api');
 const CreateWallet = require('./lib/wallet');
 
 const dbConn = require('./database/conn');
-const createWallet = new CreateWallet(config);
 
-// pre-start, check service state
-dbConn.authenticate().then(() => {
-    console.log('Wallet Service Start');
-    // start
-    return createWallet.start();
-}).catch((err) => {
-    console.log(err, 'reward');
-});
+const colors = require('colors');
 
 const Koa = require('koa');
 const Router = require('koa-router');
@@ -26,4 +18,13 @@ router.get('/', api.payCallback);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(config.server.port);
+// pre-start, check service state
+dbConn.authenticate().then(() => {
+    console.log(colors.cyan('Wallet Service Start'));
+    // start
+    app.listen(config.server.port);
+    const createWallet = new CreateWallet(config);
+    return createWallet.start();
+}).catch((err) => {
+    console.log(colors.red(err, 'reward'));
+});
